e325494a9a00a235979a926e4a1c95b121f945bf
saramotaha <saramohamedtaha909@gmail.com>
2025-04-24 18:24:53 +0200

    linked Categories in home page

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,5 +1,6 @@
 import { UserManager } from "./userManager.js";
 import { StorageManager } from "./storageManager.js";
+import { CategoryManager} from "./categoryManager.js";
 
 (function initializeDefaultAdmin() {
   UserManager.initializeDefaultAdmin();
@@ -123,3 +124,49 @@ document.addEventListener("DOMContentLoaded", () => {
   updateNavbar();
   restrictAccess();
 });
+
+
+
+
+
+
+
+// categories in home page 
+
+let categories = CategoryManager.getAllCategories();
+let categoriesActiveSlide = document.getElementById("activeSlide");
+let categoriesNextSlide = document.getElementById("nextSlide");
+
+for (let i = 0; i < 4; i++) {
+
+  categoriesActiveSlide.innerHTML +=
+    `<div class="card  cardItem p-0  position-relative  ">
+          <img src="${categories[i].image}" class="rounded w-100 " alt="...">
+    
+          <div class="cardCaption position-absolute text-center">
+            <h5 class="imgContainer text-light">${categories[i].name}</h5>
+            <div class="cardDetails text-light">
+              <p class="text-light ">0 product</p>
+              <button class=" btn p-3 btn-light ">View</button>
+            </div>
+          </div>
+        </div>`; 
+};
+
+
+for (let i = 4; i < 8; i++) {
+
+  categoriesNextSlide.innerHTML += ` <div class="card  cardItem  position-relative ">
+          <img src="${categories[i].image}" class="rounded" alt="...">
+    
+          <div class="cardCaption position-absolute text-center">
+            <h5 class="imgContainer text-light">${categories[i].name}</h5>
+            <div class="cardDetails text-light ">
+              <p class="text-light ">0 product</p>
+              <button class=" btn p-3 btn-light ">View</button>
+            </div>
+          </div>
+        </div>`;
+  
+};
+
